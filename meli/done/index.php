c061708b65a402dc77d460a5678b81d4460f420a
Daniel Atik <daniel.atik@gmail.com>
2017-09-17 15:10:53 -0300

    ..

--- meli/done/index.php
+++ meli/done/index.php
@@ -44,7 +44,7 @@ if($_SESSION['MELI_the_token']==""){
                     <p style="color:white">Ahora puedes recuperar tu <strong>access_token</strong> siempre renovado desde:</p>
                     <p><a style="color:white" href="https://simpleoauth.com/api/meli/get_token/?token=<?php echo $_SESSION['MELI_the_token']; ?>">https://simpleoauth.com/api/meli/get_token/?token=<?php echo $_SESSION['MELI_the_token']; ?></a></p>
                     <p style="color:white;">Recuerda guardar este token único de forma segura.</p>
-                    <div class="mbr-section-btn"><a class="btn btn-lg btn-danger" href="/api/meli/delete/?token=<?php echo $_SESSION['MELI_the_token']; ?>">ELIMINAR TOKEN</a> </div>
+                    <div class="mbr-section-btn"><a class="btn btn-lg btn-danger" href="/api/meli/delete/?token=<?php echo $_SESSION['MELI_the_token']; ?>">ELIMINAR ESTE TOKEN</a> </div>
                     <div class="mbr-section-btn"><a class="btn btn-lg btn-primary" href="/meli/">VOLVER</a> </div>
                     
                 </div>
