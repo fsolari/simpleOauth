1e45cfd963c5c6aa41fcc4bda5bd13845d5e979e
Daniel Atik <daniel.atik@gmail.com>
2017-09-17 15:03:43 -0300

    ..

--- api/meli/delete/index.php
+++ api/meli/delete/index.php
@@ -1,15 +1,15 @@
 <?php
 
-ini_set('display_errors', 0);
+ini_set('display_errors', 1);
 
 require '../../functions.php';
 
 
 
 // Delete unique token
-$res = deleteQuery('DELETE FROM simpleoauth.users WHERE the_token='.$_REQUEST['token'].';');
+$res = deleteQuery('DELETE FROM simpleoauth.users WHERE the_token="'.$_REQUEST['token'].'";');
 
-header('location: /');
+//header('location: /');
 
 
 ?>
