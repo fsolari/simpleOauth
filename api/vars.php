fe5b48a4027a37091b3482d9ba2477b5cff1773d
Florencia Solari <florencia.solari@mercadolibre.com>
2018-04-26 11:06:22 -0300

    Update vars.php

--- api/vars.php
+++ api/vars.php
@@ -1,16 +1,16 @@
 <?php
+
 // GLOBAL VARIABLES DEFINITIONS
 
-$dbServer="54.213.69.158";
-//$dbServer="localhost";
+$dbServer="HOST_SERVER";
 
 
-$dbUser="simpleoauth";
-$dbPass="6501ha89123987";	
-$dbName="simpleoauth";
+$dbUser="DB_USER";
+$dbPass="DB_PWD";	
+$dbName="DB_NAME";
 
 // Global app_id and secret_key
-$app_id="2220486502877433";
-$secret_key="rG3n3Lk7830EdUfjwoUuUB0wc9Sq4hQR";
+$app_id="YOUR_APP_ID";
+$secret_key="YOUR_SECRET_KEY";
 
 ?>
